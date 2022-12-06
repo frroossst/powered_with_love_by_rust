8fd51a757d18b6a8ff5e48d29b256baff4047f9f
Adhyan H <adhyanpatel@gmail.com>
2022-12-06 12:21:02 -0600

    0.2.0 - macro visibility changed

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
-macro_rules! love_rust {
-    () => {
-        println!("powered with <3 by Rust");
-    };
+#[macro_use]
+mod macros {
+    #[macro_export]
+    macro_rules! love_rust {
+        () => {
+            println!("powered with <3 by Rust");
+        };
+    }
 }
