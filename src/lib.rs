2e789e86f8cc18957b9fae388a4278a35869713b
Adhyan H <adhyanpatel@gmail.com>
2023-02-15 13:08:59 -0600

    0.3.0 - [BREAKING] takes in a string input and returns a string

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,27 @@
 mod macros {
     #[macro_export]
     macro_rules! love_rust {
-        () => {
-            println!("powered with <3 by Rust");
+        ($s: expr) => {
+            match $s {
+                "" => format!("powered with <3 by Rust"),
+                _  => format!("{}: powered with <3 by Rust", $s)
+            }
         };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn empty() {
+        assert_eq!(love_rust!(""), "powered with <3 by Rust");
+    }
+
+    #[test]
+    fn not_empty() {
+        assert_eq!(love_rust!("Rust"), "Rust: powered with <3 by Rust");
+        assert_eq!(love_rust!("oxy"), "oxy: powered with <3 by Rust");
+        assert_eq!(love_rust!("rust"), "rust: powered with <3 by Rust");
     }
 }
